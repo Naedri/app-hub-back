40ed94abb9508fccc8f3e3dfdb5fe4eec0e7cef1
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-17 17:02:15 +0200

    feat: swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('MS-users-service')
+    .setDescription('MS-users-service description')
+    .setVersion('1.0.0')
+    .addTag('MS')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
