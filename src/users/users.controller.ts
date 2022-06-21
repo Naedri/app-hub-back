77c662e07a335e4ae56fc273a05a64b4d972b9bf
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-21 15:38:50 +0200

    feat: users.controller

---
+++ src/users/users.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { JwtAuthGuard } from 'src/auth/jwt.guard';
+import { UsersService } from './users.service';
+
+@Controller('users')
+@ApiTags('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get('profile')
+  async getProfile(@Request() req): Promise<User> {
+    return await this.usersService.getUserById(req.userId);
+  }
+}
