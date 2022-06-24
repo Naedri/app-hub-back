096b920974ae6401025f9ee377f83c4e15d50046
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-24 14:34:51 +0200

    feat(apps): discover

--- src/apps/apps.service.ts
+++ src/apps/apps.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { CreateAppDto } from './dto/create-app.dto';
 import { UpdateAppDto } from './dto/update-app.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { App } from './entities/app.entity';
 
 @Injectable()
 export class AppsService {
@@ -9,11 +10,16 @@ export class AppsService {
     this.logger = new Logger(this.constructor.name);
   }
 
-  discoverOne(id: number) {
-    throw new Error('Method not implemented.');
+  async discoverOne(id: number) {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { url, ...result } = await this.findOne(id);
+    return result;
   }
-  discoverAll() {
-    throw new Error('Method not implemented.');
+
+  async discoverAll() {
+    const results = await this.findAll();
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    return results.map(({ url, ...item }) => item);
   }
 
   async create(createAppDto: CreateAppDto) {
@@ -26,7 +32,7 @@ export class AppsService {
     return result;
   }
 
-  async findAll() {
+  async findAll(): Promise<App[]> {
     let result;
     try {
       result = await this.prisma.application.findMany({});
@@ -36,7 +42,7 @@ export class AppsService {
     return result;
   }
 
-  async findOne(id: number) {
+  async findOne(id: number): Promise<App> {
     let result;
     try {
       result = await this.prisma.application.findUnique({
