451ea0d981aac1afab9b0e3861cdc149fd56a375
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-06-24 14:12:39 +0200

    fix(apps): resolve  order of endpoints

--- src/apps/apps.controller.ts
+++ src/apps/apps.controller.ts
@@ -17,8 +17,6 @@ import { AppsService } from './apps.service';
 import { CreateAppDto } from './dto/create-app.dto';
 import { UpdateAppDto } from './dto/update-app.dto';
 
-// TODO add layer security on all request with admin
-
 @Controller('apps')
 @ApiTags('apps')
 @UseGuards(MyJwtAuthGuard, RolesGuard)
@@ -26,10 +24,14 @@ import { UpdateAppDto } from './dto/update-app.dto';
 export class AppsController {
   constructor(private readonly appsService: AppsService) {}
 
-  @Post()
-  @Roles(Role.ADMIN)
-  create(@Body() createAppDto: CreateAppDto) {
-    return this.appsService.create(createAppDto);
+  @Get('discover')
+  discover() {
+    return this.appsService.discoverAll();
+  }
+
+  @Get('discover/:id')
+  discoverOne(@Param('id') id: string) {
+    return this.appsService.discoverOne(+id);
   }
 
   @Get()
@@ -44,16 +46,10 @@ export class AppsController {
     return this.appsService.findOne(+id);
   }
 
-  @Get('discover')
-  @Roles(Role.ADMIN, Role.CLIENT)
-  discover() {
-    return this.appsService.discoverAll();
-  }
-
-  @Get('discover/:id')
-  @Roles(Role.ADMIN, Role.CLIENT)
-  discoverOne(@Param('id') id: string) {
-    return this.appsService.discoverOne(+id);
+  @Post()
+  @Roles(Role.ADMIN)
+  create(@Body() createAppDto: CreateAppDto) {
+    return this.appsService.create(createAppDto);
   }
 
   @Patch(':id')
