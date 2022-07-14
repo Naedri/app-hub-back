79c3c15d85039a02ed037652db765c117a9353cd
Adrien JALLAIS <adrien.jallais@protonmail.com>
2022-07-14 13:33:08 +0200

    doc(auth): token entity description

--- src/auth/entities/token.entity.ts
+++ src/auth/entities/token.entity.ts
@@ -3,6 +3,9 @@ import { Role } from '@prisma/client';
 /**
  * Token wrapper
  */
+/**
+ * accessToken: token signed which contains data fromm TokenContentEntity
+ */
 export class TokenWrapEntity {
   accessToken: string;
 }
@@ -18,9 +21,17 @@ export class TokenContentEntity {
 }
 
 //we choose a property name of sub to hold our userId value to be consistent with JWT standards
+/**
+ * sub : hold our userId value to be consistent with JWT standards
+ * role : role of the user
+ * appId : id of the app the token is giving access
+ * subId : id of the subscription which allow the user to have access to the app
+ * subTokenUuid : uuid of the subscription token which is created to request the app
+ */
 export class AppTokenContentEntity {
-  appId: number;
   sub: number;
   role: Role;
+  appId: number;
+  subId: number;
   subTokenUuid: string;
 }
